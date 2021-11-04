c394913d8d7ba2823680d0b9d982e571c612edfb
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-11-05 01:23:07 +0300

    Add configurations

--- config.py
+++ config.py
@@ -6,9 +6,20 @@ class Config:
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/watchlist'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
-    pass
+    # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    #simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
 
 class ProdConfig(Config):
     '''
@@ -18,6 +29,10 @@ class ProdConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
     pass
+#Newly added class with new SQLALCHEMY_DATABASE_URI to connect to our test database
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:kipngeno@localhost/watchlist_test'
+
 
 class DevConfig(Config):
     '''
@@ -26,10 +41,14 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
+    #Updated so as to allow review tests updates
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:kipngeno@localhost/watchlist'
 
     DEBUG = True
 
 config_options = {
 'development':DevConfig,
-'production':ProdConfig
+'production':ProdConfig,
+'test':TestConfig
+
 }
