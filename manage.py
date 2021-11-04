287424fb3931a9765772d92d394eefff8f9bc7c5
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-11-05 01:42:58 +0300

    Add updates

--- manage.py
+++ manage.py
@@ -1,14 +1,17 @@
 from app import create_app,db
-from app.models import  Review,User
+from app.models import User,Role,Review
 from flask_script import Manager
 from flask_script import Server
+from flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
-app = create_app('development')
+app = create_app('test') #Updated so that we can use the test database URI.
 
 manager = Manager(app)
-
 manager.add_command('server',Server)
+# Create flask schema
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 @manager.command
 def test():
     """Run the unit tests."""
@@ -18,7 +21,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User )
+    return dict(app = app,db = db,User = User,Role=Role )
 
 
 if __name__ == '__main__':
